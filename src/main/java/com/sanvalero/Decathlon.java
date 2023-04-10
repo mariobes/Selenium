9816762ac5aa22ad6504f2561f2fdc359ab73bc9
mariobes <128904643+mariobes@users.noreply.github.com>
2023-04-10 19:53:38 +0200

    Programar metodo SetUp para abrir la pagina web
    Introducimos en un método SetUp el código para abrir la página web y aceptar las cookies. Este método se inicializará antes de comenzar cada test

--- src/main/java/com/sanvalero/Decathlon.java
+++ src/main/java/com/sanvalero/Decathlon.java
@@ -1,18 +1,44 @@
 package com.sanvalero;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class Decathlon {
 
+    private static WebDriver driver;
+
+    public static void SetUp() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions chromeOption = new ChromeOptions();
+        chromeOption.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(chromeOption);
+        driver.get("https://www.decathlon.es/es/");
+
+        WebElement aceptarCookies = driver.findElement(By.id("didomi-notice-agree-button"));
+        aceptarCookies.click();
+    }
+
     public String listarZapatillas() {
+
+        SetUp();
 
         return null;
     }
 
     public String buscarFutbol() {
 
+        SetUp();
+
         return null;
     }
 
     public String anadirProductoCarrito() {
+
+        SetUp();
 
         return null;
     }
